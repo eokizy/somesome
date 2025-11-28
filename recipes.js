93d8de5a6d999482355360c702a09449d64234a4
rkdxofud <109470436+eokizy@users.noreply.github.com>
2025-11-28 23:29:37 +0900

    Add files via upload

---
+++ recipes.js
@@ -0,0 +1,12 @@
+// recipes.js
+const RECIPES = [
+  {menu:'아메리카노', temp:'Hot', steps:['샷','뜨거운 물']},
+  {menu:'아메리카노', temp:'Ice', steps:['얼음','물','샷']},
+  {menu:'카페라떼', temp:'Hot', steps:['샷','스팀우유','우유폼']},
+  {menu:'카페라떼', temp:'Ice', steps:['얼음','샷','우유']},
+  {menu:'자몽에이드', temp:'Ice', steps:['자몽시럽','얼음','탄산수']},
+  {menu:'초코라떼', temp:'Hot', steps:['코코아파우더','우유']},
+  {menu:'얼그레이티', temp:'Ice', steps:['얼그레이티백', '뜨거운 물(조금)', '얼음', '물']},
+  {menu:'얼그레이티', temp:'Hot', steps:['얼그레이티백', '뜨거운 물']},
+  {menu:'딸기 연유 라떼', temp:'Ice', steps:['시럽', '산딸기리플잼', '생크림', '연유베이스', '우유', '얼음']}
+];
